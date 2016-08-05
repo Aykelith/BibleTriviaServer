b9fee84c56f3f8aad89238b9fc9cdb90911f4658
Prisacariu Alexandru <zalexxanderx@gmail.com>
2016-08-05 15:44:33 +0300

    ...

--- server.js
+++ server.js
@@ -106,6 +106,6 @@ function setNextQuestionId(matchId) {
 	};
 }
 
-server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function(){
+server.listen(process.env.PORT || 8080, '127.0.0.1', function(){
 	console.log("Listening...");
 });
